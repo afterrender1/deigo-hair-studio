583d9527a65ee7b31780caa0e0b82fe812247c91
afterrender1 <afterrender1@gmail.com>
2025-10-15 15:20:28 +0500

    nav btn updarted

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,7 +16,7 @@ export default function Navbar() {
 
   const CTAButton = (
     <a href="#contact" className="w-full sm:w-auto">
-      <button className="relative inline-flex items-center justify-center px-4 sm:px-6 md:px-2 py-2 sm:py-2.5 md:py-3 lg:py-2.5 overflow-hidden tracking-tighter text-xs sm:text-sm md:text-sm lg:text-base xl:text-lg text-white bg-sky-500 rounded-md group w-full sm:w-auto">
+      <button className="relative inline-flex items-center justify-center px-4 sm:px-6 md:px-6 py-2 sm:py-2.5 md:py-3 lg:py-3 overflow-hidden tracking-tighter text-xs sm:text-sm md:text-sm lg:text-base xl:text-lg text-white bg-sky-500 rounded-md group w-full sm:w-auto">
         <span className="absolute w-0 h-0 transition-all duration-500 ease-out bg-sky-600 rounded-full group-hover:w-56 group-hover:h-56"></span>
         <span className="absolute inset-0 w-full h-full -mt-1 rounded-lg opacity-30 bg-gradient-to-b from-transparent via-transparent to-gray-200"></span>
         <span className="relative text-xs sm:text-sm md:text-base lg:text-base xl:text-lg font-semibold">
